4c52e119a929db3e1632d139ad269ac471d28e80
CynthiaCW <cynthia.colombelwnek@gmail.com>
2022-05-30 12:53:07 +0200

    2nd page

--- src/views/CardsCollectionView.vue
+++ src/views/CardsCollectionView.vue
@@ -14,22 +14,24 @@
 
             <!-- Je crée une boucle for qui va me permettre d'afficher les 3 cartes qui se trouvent sur la home page -->
             <div v-for="(card,idx) in cards" :key="idx" class="card-list">
-                <!-- Correspond à l'id de la carte, son titre et le bouton pour voir plus de détails -->
+                 <!--Correspond à l'id de la carte, son titre et le bouton pour voir plus de détails -->
                 <div class="card-list">
+                    <div class="card-img"><img :src="card.imageUrl" style="height=105px;"></div>
                     <div class="card-title"><b>{{card.id}}</b> {{ card.name }}</div>
                     <div class="card-action"><a href="#">👀</a></div>
                 </div>
             </div>
 
+        <!--    <div v-for="card in cards" :key="card.id" class="card-list">
+                 Correspond à l'id de la carte, son titre et le bouton pour voir plus de détails 
+                <div class="card-list">
+                    <div class="card-title"><b>{{card.id}}</b> {{ card.name }}</div>
+                    <div class="card-action">👀</div>
+                </div>
+            </div>-->
 
-              <div class="card-list">
-                <div class="card-title"><b>{{card.id}}</b> {{ card.name }}</div>
-                <div class="card-action"><a href="#">👀</a></div>
-              </div>
-              <div class="card-list">
-                <div class="card-title"><b>#2</b> Card Title</div>
-                <div class="card-action"><a href="#">👀</a></div>
-              </div>
+
+              
             </article>
           </div>
         </div>
@@ -42,28 +44,28 @@
 import axios from "axios";
 
 export default {
-  name: "CardsCollection",
-  data: () => ({
-    cards: [],
-  }),
-  components: {},
-  methods: {
-    // Methods avec id de la card pour le bouton voir @click EVENT
-      goToCardsDetails(cardsId) {
-          console.log(cardsId)
-          this.$router.push({name:'DetailCardView', params:{cardsId}})
+    name: "CardsCollection",
+    data: () => ({
+        cards: [],
+    }),
+    components: {},
+    methods: {
+        // Methods avec id de la card pour le bouton voir @click EVENT
+        goToCardsDetails(cardsId) {
+            console.log(cardsId)
+            this.$router.push({name:'DetailCardView', params:{cardsId}})
         }
-      },
+    },
     async created() {
-      //On va chercher avec Get les DATA DE l'API pour toutes les cards
-      const cardsApi = await axios.get(
-        "https://api.magicthegathering.io/v1/cards "
-      );
-      //console.log(cardsApi);
-      //Axios renvoie les Data pour toutes les cards
-      this.cards = cardsApi.data.cards;
-      console.log(this.cards);
-  },
+        //On va chercher avec Get les DATA DE l'API pour toutes les cards
+        const cardsApi = await axios.get(
+            "https://api.magicthegathering.io/v1/cards "
+        );
+        //console.log(cardsApi);
+        //Axios renvoie les Data pour toutes les cards
+        this.cards = cardsApi.data.cards;
+        console.log(this.cards);
+    },
 };
 </script>
 
